9ab42f1b6722a360ba5ca633ee26224f0d1989f5
Connor Hengstler <hengstler2005@gmail.com>
2025-10-28 21:21:56 +0000

    L3 Bulkload fix

--- java-backend/src/main/resources/l3-bulkload.sh
+++ java-backend/src/main/resources/l3-bulkload.sh
@@ -3,14 +3,14 @@
 # Edit all these variable to match your GCP environment.
 # Ensure that the bulk loader .jar file is correctly named and
 # accessible by your CLI profile.
-dataproc_name="demo"
+dataproc_name="fraud-demo"
 region=us-central1
 zone=us-central1-a
 instance_type=n2d-highmem-8
 num_workers=8
-project=project-name
-bulk_jar_uri="gs://bucket-name/bulk-loader.jar"
-properties_file_uri="gs://bucket-name/properties.properties"
+project=firefly-aerospike
+bulk_jar_uri="gs://fraud-demo/jar/bulkload-3.00.jar"
+properties_file_uri="gs://fraud-demo/config/fraud.properties"
 
 # Execute the dataproc command
 gcloud dataproc clusters create "$dataproc_name" \
@@ -19,11 +19,11 @@ gcloud dataproc clusters create "$dataproc_name" \
     --zone $zone \
     --master-machine-type "$instance_type" \
     --master-boot-disk-type pd-ssd \
-    --master-boot-disk-size 25 \
+    --master-boot-disk-size 35 \
     --num-workers "$num_workers" \
     --worker-machine-type "$instance_type" \
     --worker-boot-disk-type pd-ssd \
-    --worker-boot-disk-size 25 \
+    --worker-boot-disk-size 35 \
     --image-version 2.1-debian11 \
     --properties spark:spark.history.fs.gs.outputstream.type=FLUSHABLE_COMPOSITE \
     --project $project
